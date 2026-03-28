39d1c89cc7d15b276d103d451b8836906544dcba
Bartlomiej HAGOWSKI <2510939021@hochschule-burgenland.at>
2026-03-28 11:56:04 +0100

    Initial commit

--- app.py
+++ app.py
@@ -14,7 +14,7 @@ from langchain_core.documents import Document
  
  
  
-os.environ["GOOGLE_API_KEY"] = "API_KEY" # here put your api key
+os.environ["GEMINI_API_KEY"] = os.getenv("GEMINI_API_KEY") # here put your api key
  
 # this how we create FastAPI
 app = FastAPI()
